7872c15ec6597dacd6eda8fd355baaab548acda5
Jorrit Strikwerda <jorrit.strikwerda@student.hu.nl>
2017-04-12 01:09:44 +0200

    rooster en vorige/volgende knop werkende gekregen, nu nog mn peer evaluatie....

--- goed/src/controller/MedestudentenController.java
+++ goed/src/controller/MedestudentenController.java
@@ -39,6 +39,7 @@ public class MedestudentenController implements Handler {
 	}
 
 	public void handle(Conversation conversation) {
+		SimpleDateFormat format1 = new SimpleDateFormat("dd-MM-yyyy");
 		if (conversation.getRequestedURI().startsWith("/student/medestudenten/ophalen")) {
 			ophalen(conversation);
 		} else if(conversation.getRequestedURI().startsWith("/student/rooster/ophalen")){
@@ -51,43 +52,162 @@ public class MedestudentenController implements Handler {
 				// TODO Auto-generated catch block
 				e2.printStackTrace();
 			}
+			Calendar cal = Calendar.getInstance();
+			cal.setTime(selectedDatum);
+			cal.add(Calendar.DATE, 1);
+			Date maandag = cal.getTime();
+			cal.setTime(selectedDatum);
+			cal.add(Calendar.DATE, 2);
+			Date dinsdag = cal.getTime();
+			cal.setTime(selectedDatum);
+			cal.add(Calendar.DATE, 3);
+			Date woensdag = cal.getTime();
+			cal.setTime(selectedDatum);
+			cal.add(Calendar.DATE, 4);
+			Date donderdag = cal.getTime();
+			cal.setTime(selectedDatum);
+			cal.add(Calendar.DATE, 5);
+			Date vrijdag = cal.getTime();
+			
+			
+			
+			
+			
 			Student lStudentZelf = informatieSysteem.getStudent(lGebruikersnaam);
 			String  lGroepIdZelf = lStudentZelf.getGroepId();
 			
 			Klas lKlas = informatieSysteem.getKlasVanStudent(lStudentZelf);		// klas van de student opzoeken
 			String klasCode = lKlas.getKlasCode(); // code van de klas
 
-			JsonArrayBuilder lJsonArrayBuilder = Json.createArrayBuilder();						// Uiteindelijk gaat er een array...
+			JsonArrayBuilder lJsonArrayBuilderMa = Json.createArrayBuilder();						// Uiteindelijk gaat er een array...
+			JsonArrayBuilder lJsonArrayBuilderDi = Json.createArrayBuilder();						// Uiteindelijk gaat er een array...
+			JsonArrayBuilder lJsonArrayBuilderWo = Json.createArrayBuilder();						// Uiteindelijk gaat er een array...
+			JsonArrayBuilder lJsonArrayBuilderDo = Json.createArrayBuilder();						// Uiteindelijk gaat er een array...
+			JsonArrayBuilder lJsonArrayBuilderVr = Json.createArrayBuilder();						// Uiteindelijk gaat er een array...
+			
+			JsonObjectBuilder lJsonTotaal = Json.createObjectBuilder();						// Uiteindelijk gaat er een array...
 
-			JsonObjectBuilder lJsonObjectBuilderVoorRooster = Json.createObjectBuilder(); // maak het JsonObject voor rooster
+			JsonObjectBuilder lJsonObjectBuilderVoorLesMa = Json.createObjectBuilder(); // maak het JsonObject voor rooster
+			JsonObjectBuilder lJsonObjectBuilderVoorLesDi = Json.createObjectBuilder(); // maak het JsonObject voor rooster
+			JsonObjectBuilder lJsonObjectBuilderVoorLesWo = Json.createObjectBuilder(); // maak het JsonObject voor rooster
+			JsonObjectBuilder lJsonObjectBuilderVoorLesDo = Json.createObjectBuilder(); // maak het JsonObject voor rooster
+			JsonObjectBuilder lJsonObjectBuilderVoorLesVr = Json.createObjectBuilder(); // maak het JsonObject voor rooster
+			
+			JsonObjectBuilder lJsonObjectBuilderLesMa = Json.createObjectBuilder(); // maak het JsonObject voor rooster
+			JsonObjectBuilder lJsonObjectBuilderLesDi = Json.createObjectBuilder(); // maak het JsonObject voor rooster
+			JsonObjectBuilder lJsonObjectBuilderLesWo = Json.createObjectBuilder(); // maak het JsonObject voor rooster
+			JsonObjectBuilder lJsonObjectBuilderLesDo = Json.createObjectBuilder(); // maak het JsonObject voor rooster
+			JsonObjectBuilder lJsonObjectBuilderLesVr = Json.createObjectBuilder(); // maak het JsonObject voor rooster
+			
+			JsonObjectBuilder lJsonMaandag = Json.createObjectBuilder(); // maak het JsonObject voor rooster
+			JsonObjectBuilder lJsonDinsdag = Json.createObjectBuilder(); // maak het JsonObject voor rooster
+			JsonObjectBuilder lJsonWoensdag = Json.createObjectBuilder(); // maak het JsonObject voor rooster
+			JsonObjectBuilder lJsonDonderdag = Json.createObjectBuilder(); // maak het JsonObject voor rooster
+			JsonObjectBuilder lJsonVrijdag = Json.createObjectBuilder(); // maak het JsonObject voor rooster
 			
 			String csvFile = "././CSV/rooster.csv";
 			BufferedReader br = null;
 			String line = "";
+			ArrayList data = new ArrayList<Date>();
 			try {
 				br = new BufferedReader(new FileReader(csvFile));
 				while ((line = br.readLine()) != null) {
 						String[] element = line.split(",");
 						String klas = element[6];
 						Date yourDate = new SimpleDateFormat("yyyy-MM-dd").parse(element[0]);
-						Calendar c = Calendar.getInstance();
-						c.setTime(yourDate);
-						int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
-						if(klasCode.equals(klas)){
-							lJsonObjectBuilderVoorRooster.add("datum", element[0]);
-							
-							lJsonObjectBuilderVoorRooster.add("startTijd", element[1]);
-							lJsonObjectBuilderVoorRooster.add("eindTijd", element[2]);
-							lJsonObjectBuilderVoorRooster.add("klas", element[3]);
-							lJsonObjectBuilderVoorRooster.add("docent", element[4]);
-							lJsonObjectBuilderVoorRooster.add("locatie", element[5]);
-							lJsonObjectBuilderVoorRooster.add("klasCode", element[6]);
-						lJsonArrayBuilder.add(lJsonObjectBuilderVoorRooster);
+						if(!data.contains(yourDate)){
+							data.add(yourDate);
 						}
+						if(yourDate.equals(maandag)){
+							if(klasCode.equals(klas)){							
+								lJsonObjectBuilderVoorLesMa.add("startTijd", element[1]);
+								lJsonObjectBuilderVoorLesMa.add("eindTijd", element[2]);
+								lJsonObjectBuilderVoorLesMa.add("vak", element[3]);
+								lJsonObjectBuilderVoorLesMa.add("emailDocent", element[4]);
+								lJsonObjectBuilderVoorLesMa.add("locatie", element[5]);
+								lJsonObjectBuilderVoorLesMa.add("klas", element[6]);
+							lJsonObjectBuilderLesMa.add("les",lJsonObjectBuilderVoorLesMa);
+							lJsonArrayBuilderMa.add(lJsonObjectBuilderLesMa);
+							}
+						}
+						if(yourDate.equals(dinsdag)){
+							if(klasCode.equals(klas)){							
+								lJsonObjectBuilderVoorLesDi.add("startTijd", element[1]);
+								lJsonObjectBuilderVoorLesDi.add("eindTijd", element[2]);
+								lJsonObjectBuilderVoorLesDi.add("vak", element[3]);
+								lJsonObjectBuilderVoorLesDi.add("emailDocent", element[4]);
+								lJsonObjectBuilderVoorLesDi.add("locatie", element[5]);
+								lJsonObjectBuilderVoorLesDi.add("klas", element[6]);
+							lJsonObjectBuilderLesDi.add("les",lJsonObjectBuilderVoorLesDi);
+							lJsonArrayBuilderDi.add(lJsonObjectBuilderLesDi);
+							}
+						}
+						if(yourDate.equals(woensdag)){
+							if(klasCode.equals(klas)){							
+								lJsonObjectBuilderVoorLesWo.add("startTijd", element[1]);
+								lJsonObjectBuilderVoorLesWo.add("eindTijd", element[2]);
+								lJsonObjectBuilderVoorLesWo.add("vak", element[3]);
+								lJsonObjectBuilderVoorLesWo.add("emailDocent", element[4]);
+								lJsonObjectBuilderVoorLesWo.add("locatie", element[5]);
+								lJsonObjectBuilderVoorLesWo.add("klas", element[6]);
+							lJsonObjectBuilderLesWo.add("les",lJsonObjectBuilderVoorLesWo);
+							lJsonArrayBuilderWo.add(lJsonObjectBuilderLesWo);
+							}
+						}
+						if(yourDate.equals(donderdag)){
+							if(klasCode.equals(klas)){							
+								lJsonObjectBuilderVoorLesDo.add("startTijd", element[1]);
+								lJsonObjectBuilderVoorLesDo.add("eindTijd", element[2]);
+								lJsonObjectBuilderVoorLesDo.add("vak", element[3]);
+								lJsonObjectBuilderVoorLesDo.add("emailDocent", element[4]);
+								lJsonObjectBuilderVoorLesDo.add("locatie", element[5]);
+								lJsonObjectBuilderVoorLesDo.add("klas", element[6]);
+							lJsonObjectBuilderLesDo.add("les",lJsonObjectBuilderVoorLesDo);
+							lJsonArrayBuilderDo.add(lJsonObjectBuilderLesDo);
+							}
+						}
+						if(yourDate.equals(vrijdag)){
+							if(klasCode.equals(klas)){							
+								lJsonObjectBuilderVoorLesVr.add("startTijd", element[1]);
+								lJsonObjectBuilderVoorLesVr.add("eindTijd", element[2]);
+								lJsonObjectBuilderVoorLesVr.add("vak", element[3]);
+								lJsonObjectBuilderVoorLesVr.add("emailDocent", element[4]);
+								lJsonObjectBuilderVoorLesVr.add("locatie", element[5]);
+								lJsonObjectBuilderVoorLesVr.add("klas", element[6]);
+							lJsonObjectBuilderLesVr.add("les",lJsonObjectBuilderVoorLesVr);
+							lJsonArrayBuilderVr.add(lJsonObjectBuilderLesVr);
+							}
+						}
+							
+						
+						
+						
+
 						
 				}	
-			   String lJsonOutStr = lJsonArrayBuilder.build().toString();												// maak er een string van
-			   System.out.println(lJsonOutStr);
+				 lJsonMaandag.add("datum", format1.format(maandag));
+				 lJsonMaandag.add("lesgegevens", lJsonArrayBuilderMa);
+				 
+				 lJsonDinsdag.add("datum", format1.format(dinsdag));
+				 lJsonDinsdag.add("lesgegevens", lJsonArrayBuilderDi);
+				 
+				 lJsonWoensdag.add("datum", format1.format(woensdag));
+				 lJsonWoensdag.add("lesgegevens", lJsonArrayBuilderWo);
+				 
+				 lJsonDonderdag.add("datum", format1.format(donderdag));
+				 lJsonDonderdag.add("lesgegevens", lJsonArrayBuilderDo);
+				 
+				 lJsonVrijdag.add("datum", format1.format(vrijdag));
+				 lJsonVrijdag.add("lesgegevens", lJsonArrayBuilderVr);
+				 
+				 lJsonTotaal.add("maandag", lJsonMaandag);
+				 lJsonTotaal.add("dinsdag", lJsonDinsdag);
+				 lJsonTotaal.add("woensdag", lJsonWoensdag);
+				 lJsonTotaal.add("donderdag", lJsonDonderdag);
+				 lJsonTotaal.add("vrijdag", lJsonVrijdag);
+				 String jsonmaandag = lJsonMaandag.build().toString();	
+				 String jsondinsdag = lJsonDinsdag.build().toString();	
+			   String lJsonOutStr = lJsonTotaal.build().toString();												// maak er een string van
 			   conversation.sendJSONMessage(lJsonOutStr);		
 			} catch (FileNotFoundException e1) {
 				// TODO Auto-generated catch block
@@ -154,7 +274,6 @@ public class MedestudentenController implements Handler {
 					.add("firstName", lMedeStudent.getVoornaam())	
 					.add("lastName", lLastName)				     
 				  .add("sameGroup", lZelfdeGroep);	
-					System.out.println(lLastName);
 			  
 			  lJsonArrayBuilder.add(lJsonObjectBuilderVoorStudent);													//voeg het JsonObject aan het array toe				     
 			}
