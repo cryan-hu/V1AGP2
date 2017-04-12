f22787d0e82a108c496b966f20587f7961165d9e
CK <ck.tsang@hotmail.com>
2017-04-12 12:00:51 +0200

    Final

--- goed/src/controller/Application.java
+++ goed/src/controller/Application.java
@@ -27,7 +27,7 @@ public class Application {
 	 * 
 	 */
 	public static void main(String[] args) {
-		JSONFileServer server = new JSONFileServer(new File("webapp/app"), 8880);
+		JSONFileServer server = new JSONFileServer(new File("webapp/app"), 8280);
 		
 		PrIS infoSysteem = new PrIS();
 		
